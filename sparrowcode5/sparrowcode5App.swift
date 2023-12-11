35f215810da3a7604590758b2cf21b599c5e95c4
Artem <adelelain@gmail.com>
2023-12-11 14:09:21 +0300

    Initial Commit

---
+++ sparrowcode5/sparrowcode5App.swift
@@ -0,0 +1,17 @@
+//
+//  sparrowcode5App.swift
+//  sparrowcode5
+//
+//  Created by Artem Dragunov on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct sparrowcode5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
