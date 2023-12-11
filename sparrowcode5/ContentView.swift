ae6bb256a8f7c1c0889fb03ef31b848d0c0bb679
Artem <adelelain@gmail.com>
2023-12-11 23:54:29 +0300

    Update ContentView.swift

--- sparrowcode5/ContentView.swift
+++ sparrowcode5/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            Rectangle()
+                .fill(.white)
+            Rectangle()
+                .fill(.pink)
+            Rectangle()
+                .fill(.yellow)
+            Rectangle()
+                .fill(.black)
+            
         }
-        .padding()
+        .padding(0)
+    
     }
 }
 
